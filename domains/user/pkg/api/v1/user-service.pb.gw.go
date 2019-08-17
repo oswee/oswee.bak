9669decf8e086644a37121b91f4bcad87f67d630
oswee <dzintars.klavins@gmail.com>
2019-08-17 13:32:13 +0300

    Updates user proto

--- domains/user/pkg/api/v1/user-service.pb.gw.go
+++ domains/user/pkg/api/v1/user-service.pb.gw.go
@@ -45,6 +45,23 @@ func request_UserCommandService_CreateSignup_0(ctx context.Context, marshaler ru
 
 }
 
+func request_UserQueryService_SignIn_0(ctx context.Context, marshaler runtime.Marshaler, client UserQueryServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq SignInRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.SignIn(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterUserCommandServiceHandlerFromEndpoint is same as RegisterUserCommandServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterUserCommandServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -112,4 +129,73 @@ var (
 
 var (
 	forward_UserCommandService_CreateSignup_0 = runtime.ForwardResponseMessage
+)
+
+// RegisterUserQueryServiceHandlerFromEndpoint is same as RegisterUserQueryServiceHandler but
+// automatically dials to "endpoint" and closes the connection when "ctx" gets done.
+func RegisterUserQueryServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
+	conn, err := grpc.Dial(endpoint, opts...)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+			}
+			return
+		}
+		go func() {
+			<-ctx.Done()
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+			}
+		}()
+	}()
+
+	return RegisterUserQueryServiceHandler(ctx, mux, conn)
+}
+
+// RegisterUserQueryServiceHandler registers the http handlers for service UserQueryService to "mux".
+// The handlers forward requests to the grpc endpoint over "conn".
+func RegisterUserQueryServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error {
+	return RegisterUserQueryServiceHandlerClient(ctx, mux, NewUserQueryServiceClient(conn))
+}
+
+// RegisterUserQueryServiceHandlerClient registers the http handlers for service UserQueryService
+// to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "UserQueryServiceClient".
+// Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "UserQueryServiceClient"
+// doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
+// "UserQueryServiceClient" to call the correct interceptors.
+func RegisterUserQueryServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client UserQueryServiceClient) error {
+
+	mux.Handle("POST", pattern_UserQueryService_SignIn_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_UserQueryService_SignIn_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_UserQueryService_SignIn_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	return nil
+}
+
+var (
+	pattern_UserQueryService_SignIn_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "signin"}, ""))
+)
+
+var (
+	forward_UserQueryService_SignIn_0 = runtime.ForwardResponseMessage
 )
